24561fd7fe3aa8521e604c6c2777bc5786d69849
Minsup Ju <me@ptcookie.net>
2026-05-20 01:05:36 +0900

    chore: Linter and plugins
    Setup ESLint.
    Add Astro plugins.

---
+++ eslint.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'node:path';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
+import pluginAstro from 'eslint-plugin-astro';
+import pluginReact from '@eslint-react/eslint-plugin';
+
+export default defineConfig([
+  includeIgnoreFile(resolve(import.meta.dirname, '.gitignore')),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tsEslint.configs.strict,
+  tsEslint.configs.stylistic,
+  pluginAstro.configs.recommended,
+  {
+    files: ['**/*.{jsx,tsx}'],
+    ...pluginReact.configs.strict
+  },
+  {
+    ...stylistic.configs.customize({ semi: true }),
+    rules: { '@stylistic/max-len': ['warn', { code: 120 }] }
+  },
+]);
